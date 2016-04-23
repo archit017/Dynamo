e54a91011dc6a6c03294aadf532214224046a1d8
Girish Gokul <girishg1192@gmail.com>
2016-04-23 17:17:18 -0400

    Hash list of all nodes

--- app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SimpleDynamoProvider.java
@@ -1,16 +1,73 @@
 package edu.buffalo.cse.cse486586.simpledynamo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Formatter;
+import java.util.HashMap;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 public class SimpleDynamoProvider extends ContentProvider {
 
+    String TAG = "SimpleDynamoProvider";
+	ArrayList<String> nodeHashList;
+    HashMap<String, Integer> nodePortMap;
+    private int myPort;
+    private String localNodeID;
+    private static String delim = "`";
+    static final int SERVER_PORT = 10000;
+    @Override
+    public boolean onCreate(){
+        TelephonyManager tel = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+        Log.v(TAG, tel.getLine1Number());
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        myPort = (Integer.parseInt(portStr) * 2);
+        localNodeID = genHash(String.valueOf(myPort / 2));
+        Log.v(TAG, "PORT: " + myPort);
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            Log.e(TAG, "Can't create a ServerSocket");
+            e.printStackTrace();
+        }
+
+        //Populate node IDs
+        nodeHashList = new ArrayList<String>();
+        nodePortMap = new HashMap<String, Integer>();
+        int[] ports = {5554,5556,5558, 5560, 5562};
+        for (int port:
+             ports) {
+            String hash = genHash(String.valueOf(port));
+            nodeHashList.add(hash);
+            nodePortMap.put(hash, Integer.valueOf(port*2));
+        }
+        Collections.sort(nodeHashList);
+        Log.v(TAG, "Node hash space" + nodeHashList);
+        Log.v(TAG, "ports" + nodePortMap);
+
+        //get next two neighbours
+        int i = nodeHashList.indexOf(genHash(String.valueOf(myPort/2)));
+        Log.v(TAG, "index " + i);
+        nodePortMap.get(nodeHashList.get((i + 1) % 5));
+        Log.v(TAG, "neighbours " +  nodePortMap.get(nodeHashList.get((i + 1) % 5)) + " " +  nodePortMap.get(nodeHashList.get((i + 2) % 5)));
+        return false;
+    }
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		// TODO Auto-generated method stub
@@ -29,11 +86,6 @@ public class SimpleDynamoProvider extends ContentProvider {
 		return null;
 	}
 
-	@Override
-	public boolean onCreate() {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
@@ -49,13 +101,47 @@ public class SimpleDynamoProvider extends ContentProvider {
 		return 0;
 	}
 
-    private String genHash(String input) throws NoSuchAlgorithmException {
-        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-        byte[] sha1Hash = sha1.digest(input.getBytes());
-        Formatter formatter = new Formatter();
-        for (byte b : sha1Hash) {
-            formatter.format("%02x", b);
+    private String genHash(String input){
+        MessageDigest sha1 = null;
+        try {
+            sha1 = MessageDigest.getInstance("SHA-1");
+            byte[] sha1Hash = sha1.digest(input.getBytes());
+            Formatter formatter = new Formatter();
+            for (byte b : sha1Hash) {
+                formatter.format("%02x", b);
+            }
+            return formatter.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
-        return formatter.toString();
+        return null;
     }
+
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+            do {
+                try {
+                    Socket clientHook = serverSocket.accept();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientHook.getInputStream()));
+                    String message = reader.readLine();
+                    Log.v(TAG, "Server " + message);
+                    String[] args = message.split(delim);
+
+
+                    clientHook.close();
+                    //serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            } while (true);
+        }
+
+        protected void onProgressUpdate(String... strings) {
+            //Nothin to do here
+        }
+    }
+    
 }
